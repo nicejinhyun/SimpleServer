60de0f8d43db5f0550a99b37eab87bca9a25f288
nicejinhyun <nicejinhyun@gmail.com>
2023-09-22 14:06:00 +0900

    update

--- run.sh
+++ run.sh
@@ -1,17 +1,3 @@
-#!/bin/sh
+#!/usr/bin/with-contenv bashio
 
-SHARE_DIR=/share/SimpleServer
-
-if [ ! -f $SHARE_DIR/server.py ]; then
-	mkdir $SHARE_DIR
-	mv /server.py $SHARE_DIR
-    mv /client.py $SHARE_DIR
-fi
-/makeconf.sh
-
-echo "[Info] Simple Server"
-cd $SHARE_DIR
-python3 $SHARE_DIR/server.py
-
-# for dev
-while true; do echo "still live"; sleep 100; done
+echo "Hello world!"
